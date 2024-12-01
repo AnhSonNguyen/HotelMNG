6204b42526629fbaa02f03d58638a5217390f608
AnhSonNguyen <anhsonnguyen1806@gmail.com>
2024-12-02 00:27:13 +0700

    thêm hiển thị danh sách phòng, ghép template admin

---
+++ HotelMNG/ViewComponents/RoomsViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using HotelMNG.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelMNG.ViewComponents
+{
+    public class RoomsViewComponent : ViewComponent
+    {
+        private readonly Qlks3Context _context;
+
+        public RoomsViewComponent(Qlks3Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = await _context.Rooms
+                .OrderByDescending(m => m.RoomId)
+                .ToListAsync();
+
+            return View(items);
+        }
+    }
+}
