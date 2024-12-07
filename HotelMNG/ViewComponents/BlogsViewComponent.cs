a32c21ee53265aa9489a92099e17b78a85fea940
AnhSonNguyen <anhsonnguyen1806@gmail.com>
2024-12-07 15:14:46 +0700

    Sửa lỗi vặt

---
+++ HotelMNG/ViewComponents/BlogsViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using HotelMNG.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelMNG.ViewComponents
+{
+    public class BlogsViewComponent : ViewComponent
+    {
+        private readonly Qlks3Context _context;
+
+        public BlogsViewComponent(Qlks3Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = await _context.BlogPosts
+                .OrderByDescending(b => b.CreatedAt)
+                .ToListAsync();
+
+            return View(items);
+        }
+    }
+}
