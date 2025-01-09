8027d7ff81ea6dfca63f65f777bc52dbadcefc2e
AnhSonNguyen <anhsonnguyen1806@gmail.com>
2025-01-09 18:39:30 +0700

    Thêm mục quản lý, thay ảnh

---
+++ HotelMNG/Areas/Admin/Controllers/FileManagerController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelMNG.Areas.Admin.Controllers
+{
+    public class FileManagerController : Controller
+    {
+        [Area("Admin")]
+        [Route("/Admin/file-manager")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
